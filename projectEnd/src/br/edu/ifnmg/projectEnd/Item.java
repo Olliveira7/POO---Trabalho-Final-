c596f1e3d0705ad1358219549d5298ad85082b01
Olliveira7 <diegoolliveira1999@gmail.com>
2019-11-19 18:24:17 -0300

    Criando classes

--- projectEnd/src/br/edu/ifnmg/projectEnd/Item.java
+++ projectEnd/src/br/edu/ifnmg/projectEnd/Item.java
@@ -5,10 +5,42 @@
  */
 package br.edu.ifnmg.projectEnd;
 
-/**
- *
- * @author Diego
- */
+
+
 public class Item {
+    private Product product;
+    //A minha unidade deve ter uma enumeração
+    private int amount;
+    private float value;
     
+    public Item(){
+        this.product = new Product();
+        // Inicializar a unidade
+        this.amount = 0;
+        this.value = 0;
+    }
+    
+    public Product getProduct(){
+        return this.product;
+    }
+    
+    public void setProduct(Product newProduct){
+        this.product = newProduct;
+    }
+    
+    public int getAmount(){
+        return this.amount;
+    }
+    
+    public void setAmount(int newAmount){
+        this.amount = newAmount;
+    }
+    
+    public float getValue(){
+        return this.value;
+    }
+    
+    public void setValue(float newValue){
+        this.value = newValue;
+    }
 }
--- projectEnd/src/br/edu/ifnmg/projectEnd/Sale.java
+++ projectEnd/src/br/edu/ifnmg/projectEnd/Sale.java
@@ -5,10 +5,61 @@
  */
 package br.edu.ifnmg.projectEnd;
 
-/**
- *
- * @author Diego
- */
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+
 public class Sale {
     
+    private float value_total;
+    private List<Item> itens;
+    private Date date;
+    private Client client;
+    
+    //tem q ver como manipular o Date
+    
+    public Sale(){
+        this.value_total = 0;
+        this.client = new Client();
+        this.date = new Date();
+        this.itens = new ArrayList<Item>();
+    }
+    
+    public float getValue_total(){
+        return this.value_total;
+    }
+    
+    public void setValue_total(float newValue){
+        this.value_total = newValue;
+    }
+    
+    public Client getClient(){
+        return this.client;
+    }
+    
+    public void setUser(Client newClient){
+        this.client = newClient;
+    }
+    
+    public Date getDate(){
+        return this.date;
+    }
+    
+    public void setDate(Date newDate){
+        this.date = newDate;
+    }
+    
+    public void addItem(Item newItem){
+        if(newItem != null){
+            this.itens.add(newItem);
+        }
+    }
+    
+    public void removeItem(Item newItem){
+        if(this.itens.contains(newItem))
+            this.itens.remove(newItem);
+    }
+   
 }
